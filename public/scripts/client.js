fee142ee207e19e97f4b1a22600ffc2fce97920d
wangxin <xinwang213z@gmail.com>
2022-03-31 02:09:35 +0000

    Prevent Cross-Site Scripting and Displaying Validation Errors

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,77 +5,81 @@
  */
 
 $(() => {
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
-
-  //fetching tweets 
+  $("#arrow").animate({ bottom: "-=200px" }, "slow", () => {
+    $("#arrow").animate({ top: "-=200px" });
+  });
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+  //fetching tweets and render html
   const loadTweets = function () {
-    
-  }
+    $.ajax({ url: "/tweets", method: "GET" }).then(function (data) {
+      renderTweets(data);
+    });
+  };
+  loadTweets();
 
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
-  const renderTweets = function (tweets) {
+  function renderTweets(tweets) {
     const $tweetsContainer = $("#tweets-container");
     for (const tweet of tweets) {
       const $newtweet = createTweetElement(tweet);
-      $tweetsContainer.append($newtweet);
+      $tweetsContainer.prepend($newtweet);
     }
     return $tweetsContainer;
-  };
+  }
 
   //return a tweet element
   function createTweetElement(tweetData) {
     const $tweet = $(`<article id="tweet">
-  <header class = "all-tweet-header">
+    <header class = "all-tweet-header">
            <div class = "all-tweet-header-left">
-            <img src=${tweetData.user.avatars}> 
-            <a rel="author">${tweetData.user.name}</a>
+            <img src=${escape(tweetData.user.avatars)}> 
+            <a rel="author">${escape(tweetData.user.name)}</a>
            </div>
-           <a rel="author">${tweetData.user.handle}</a>
+           <a rel="author">${escape(tweetData.user.handle)}</a>
           </header>
           <div class = "all-tweet-content">
-           <p>${tweetData.content.text}</p>
+           <p>${escape(tweetData.content.text)}</p>
           </div>
           <footer class = "all-tweet-footer">
-            <time>${tweetData.created_at}</time>
+            <time>${escape(timeago.format(tweetData.created_at))}</time>
             <div id = "logo">
              <i id = "flag" class="fa-solid fa-flag"></i>
              <i id = "retweet" class="fa-solid fa-arrow-up-right-from-square"></i>
              <i id = "heart" class="fa-solid fa-heart"></i>
             </div>
           </footer>
-  </article>`);
+     </article>`);
     return $tweet;
   }
-  renderTweets(data);
 
-  $("#target").submit(function (event) {
-    console.log($(this));
-    event.preventDefault();
-    $.ajax({ url: "/tweets", method: "POST", data: $(this).serialize() });
-  });
+  // Submit a form use ajax
+  function sendTweet() {
+    $("#target").submit(function (event) {
+      event.preventDefault();
+      const data = $(this).serialize();
+      const $tweetValue = $($(this).children()[1]);
+      if ($tweetValue.val().length > 140) {
+        $(".error").text("Too long.Please limit your text of 140 chars.");
+        $(".error").slideDown("slow");
+        return;
+      }
+      if ($tweetValue.val().length === 0) {
+        $(".error").text("Text can't be empty");
+        $(".error").slideDown("slow");
+        return;
+      }
+      $(".error").slideUp("slow");
+      $.ajax({ url: "/tweets", method: "POST", data: data }).then(() => {
+        loadTweets();
+      });
+      $tweetValue.val(null);
+    });
+  }
+  sendTweet();
 });
